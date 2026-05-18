787713c9b32050418fb9dae20882456b0591918c
Łukasz Piątkowski <piontec@gmail.com>
2026-05-18 18:29:19 +0200

    Update .config/nvim/init.lua Update .config/nvim/lua/config/autocmds.lua Update .config/nvim/lua/config/options.lua Update .config/nvim/lua/plugins/config_nvim-lspconfig.lua

--- chezmoi/private_dot_config/nvim/lua/plugins/config_nvim-lspconfig.lua
+++ chezmoi/private_dot_config/nvim/lua/plugins/config_nvim-lspconfig.lua
@@ -1,26 +1,11 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    ---@class PluginLspOpts
     opts = {
-      ---@type lspconfig.options
       servers = {
         bashls = {},
-        dagger = {},
         dockerls = {},
-        gopls = {},
         kcl = {},
-        marksman = {},
-        pyright = {},
-        tflint = {},
-        terraformls = {},
-        helm_ls = {
-          yamlls = {
-            settings = {
-              path = "yaml-language-server",
-            },
-          },
-        },
       },
     },
   },
