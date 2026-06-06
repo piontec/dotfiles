b21ecd57766fe735410453fa783068d678672572
Łukasz Piątkowski <piontec@gmail.com>
2026-06-06 11:02:35 +0200

    Update backup/bin/sync-backup.sh Update .zshrc

--- chezmoi/backup/bin/executable_sync-backup.sh
+++ chezmoi/backup/bin/executable_sync-backup.sh
@@ -10,18 +10,17 @@ EBOOKS_SRC_DIR="/mnt/data/ebooks"
 EBOOKS_RSYNC="$RSYNC_BASE/ebooks"
 PRIVATE_SRC_DIR="/home/piontec/backup"
 PRIVATE_3D_SRC_DIR_1="/mnt/data/diy/laser"
-PRIVATE_3D_SRC_DIR_2="/mnt/data/diy/freecad"
-PRIVATE_RSYNC="$RSYNC_BASE/backup/piontec-t495"
+PRIVATE_RSYNC="$RSYNC_BASE/backup/piontec"
 IF=wlp98s0
 
 sync_dir() {
-	SRC_DIR=$1
-	RMT_DIR=$2
-	FILTER=$3
-	EXTRA=$4
-	echo "Syncing from $SRC_DIR to $RMT_DIR with filter $FILTER"
-	[ ! -d "$SRC_DIR" ] && echo "Directory $SRC_DIR DOES NOT exists." && exit 1
-	rsync --bwlimit=30M -rlptoDvxP "$SRC_DIR/" "$RMT_DIR/" --include="$FILTER" --exclude="*" $EXTRA
+    SRC_DIR=$1
+    RMT_DIR=$2
+    FILTER=$3
+    EXTRA=$4
+    echo "Syncing from $SRC_DIR to $RMT_DIR with filter $FILTER"
+    [ ! -d "$SRC_DIR" ] && echo "Directory $SRC_DIR DOES NOT exists." && exit 1
+    rsync --bwlimit=30M -rlptoDvxP "$SRC_DIR/" "$RMT_DIR/" --include="$FILTER" --exclude="*" $EXTRA
 }
 
 uid=$(id -u $USER)
@@ -30,8 +29,8 @@ export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/${uid}/bus
 connection_name=$(nmcli device show $IF | awk '/GENERAL\.CONNECTION/ {print $2}')
 connection_state=$(nmcli device show $IF | awk -F "[()]" '/GENERAL\.STATE/ {print $2}')
 if [[ ! (((("$connection_name" == "konet" || "$connection_name" == "konet5")) && "$connection_state" == "connected")) ]]; then
-	notify-send "Not doing backups as not connected to 'konet'or 'konet5'"
-	exit 1
+    notify-send "Not doing backups as not connected to 'konet'or 'konet5'"
+    exit 1
 fi
 
 notify-send "Starting rsync backup"
@@ -49,26 +48,25 @@ sync_dir $EBOOKS_SRC_DIR $EBOOKS_RSYNC "/_import/***" --delete
 # private backup
 sync_dir $PRIVATE_SRC_DIR $PRIVATE_RSYNC "/***"
 sync_dir $PRIVATE_3D_SRC_DIR_1 $PRIVATE_RSYNC/diy/laser "/***"
-sync_dir $PRIVATE_3D_SRC_DIR_2 $PRIVATE_RSYNC/diy/freecad "/***"
 # done
 API_KEY="$(cat /home/piontec/.immich-key)"
 immich-go upload from-folder \
-	--no-ui \
-	--folder-as-album FOLDER \
-	--manage-raw-jpeg=StackCoverJPG \
-	--server http://immich:2283 \
-	--api-key "$API_KEY" \
-	--admin-api-key "$(cat /home/piontec/.immich-key-admin)" \
-	--manage-raw-jpeg StackCoverJPG \
-	--manage-burst Stack /mnt/data/photos/my*
+    --no-ui \
+    --folder-as-album FOLDER \
+    --manage-raw-jpeg=StackCoverJPG \
+    --server http://immich:2283 \
+    --api-key "$API_KEY" \
+    --admin-api-key "$(cat /home/piontec/.immich-key-admin)" \
+    --manage-raw-jpeg StackCoverJPG \
+    --manage-burst Stack /mnt/data/photos/my*
 immich-go upload from-folder \
-	--no-ui \
-	--folder-as-album FOLDER \
-	--manage-raw-jpeg=StackCoverJPG \
-	--server http://immich:2283 \
-	--api-key "$API_KEY" \
-	--admin-api-key "$(cat /home/piontec/.immich-key-admin)" \
-	--manage-raw-jpeg StackCoverJPG \
-	--manage-burst Stack /mnt/data/photos/mix*
+    --no-ui \
+    --folder-as-album FOLDER \
+    --manage-raw-jpeg=StackCoverJPG \
+    --server http://immich:2283 \
+    --api-key "$API_KEY" \
+    --admin-api-key "$(cat /home/piontec/.immich-key-admin)" \
+    --manage-raw-jpeg StackCoverJPG \
+    --manage-burst Stack /mnt/data/photos/mix*
 node ~/backup/bin/immich-add-user-to-all-albums.js http://immich:2283 "$API_KEY" krystyna.m.piatkowska@gmail.com editor
 notify-send "Rsync backup done"
